8d86387623c3ac8d60897c46764481cf7e2c3118
ArianeAlix <33422511+ArianeAlix@users.noreply.github.com>
2020-04-22 16:49:11 +0200

    Add files via upload

---
+++ Image processing/Code/gram.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+import torch
+
+class GramMatrix(nn.Module):
+    def forward(self, input):
+        b,c,h,w = input.size()
+        F = input.view(b, c, h*w)
+        G = torch.bmm(F, F.transpose(1,2)) 
+        G.div_(h*w)
+        return G
+
+class GramMSELoss(nn.Module):
+    def forward(self, input, target):
+        out = nn.MSELoss()(GramMatrix()(input), target)
+        return(out)
---
+++ Image processing/Code/processing.py
@@ -0,0 +1,30 @@
+import torch
+import torchvision
+from torchvision import transforms
+
+
+# pre and post processing for images
+img_size = 512 
+prep = transforms.Compose([transforms.Resize(img_size),
+                           transforms.ToTensor(),
+                           transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
+                           transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961], #subtract imagenet mean
+                                                std=[1,1,1]),
+                           transforms.Lambda(lambda x: x.mul_(255)),
+                          ])
+                          
+                          
+postpa = transforms.Compose([transforms.Lambda(lambda x: x.mul_(1./255)),
+                           transforms.Normalize(mean=[-0.40760392, -0.45795686, -0.48501961], #add imagenet mean
+                                                std=[1,1,1]),
+                           transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to RGB
+                           ])
+                           
+postpb = transforms.Compose([transforms.ToPILImage()])
+
+def postp(tensor): # to clip results in the range [0,1]
+    t = postpa(tensor)
+    t[t>1] = 1    
+    t[t<0] = 0
+    img = postpb(t)
+    return img
